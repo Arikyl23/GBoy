0fcc028f2dc0b8ce167ba8b9d9c5eaa46ed23b62
Arikyl23 <60128605+Arikyl23@users.noreply.github.com>
2026-01-16 09:30:56 -0700

    docs: Added missing doxygen comments and fixed other related ones

--- src/memory/cartridge/mbc/mbc1.h
+++ src/memory/cartridge/mbc/mbc1.h
@@ -9,6 +9,21 @@
 
 #include "memory/cartridge/mbc/mbc_iface.h"
 
+/**
+ * @brief Fetches the MBC interface for a MBC1 child object.
+ * @returns A constant mbc_iface pointer to static memory containing the interface.
+ */
 const struct mbc_iface* mbc1_iface(void);
+/**
+ * @brief Instanciation of a new MBC1 type MBC.
+ * @param n_rom_banks Specifies the number of ROM banks this MBC1 controls. Must be [1, 128]
+ * @param n_ram_banks Specifies the number of RAM banks this MBC1 controls. Must be [0,4].
+ * @returns A pointer to the created MBC1 context. A value of `NULL` means the instancation failed.
+ */
 void*                   mbc1_instanciate(const size_t n_rom_banks, const size_t n_ram_banks);
+/**
+ * @brief Destroy an instance of a MBC1 type MBC.
+ * @param p_ctx Pointer to a MBC1 context pointer. Must not be `NULL`.
+ * @note This sets the MBC1 context pointer to `NULL` destroying its internal memory.
+ */
 void                    mbc1_destroy(void** p_ctx);
