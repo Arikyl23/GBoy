437468c6171d7059a329bd86a9e5d5d58fb4da0d
Arikyl23 <60128605+Arikyl23@users.noreply.github.com>
2026-06-14 00:53:10 -0600

    Added SUB opcodes

--- src/cpu/cpu.c
+++ src/cpu/cpu.c
@@ -28,23 +28,23 @@ static word cpu_read_u16(void);
 
 // clang-format off
 static const CPU_INSTR m_opcode_table[0x100] = {
-        // 0x_0                | 0x_1                 | 0x_2                 | 0x_3                | 0x_4                  | 0x_5                | 0x_6                | 0x_7                | 0x_8                | 0x_9                | 0x_A                 | 0x_B                | 0x_C                 | 0x_D                | 0x_E                 | 0x_F
-/* 0x0_ */ cpu_opcode_NOP,       cpu_opcode_LD_rBC_u16, cpu_opcode_LD_iBC_rA,  cpu_opcode_INC_rBC,   cpu_opcode_INC_rB,      cpu_opcode_DEC_rB,    cpu_opcode_LD_rB_u8,  cpu_opcode_RLCA,      cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_LD_rA_iBC,  cpu_opcode_undefined, cpu_opcode_INC_rC,     cpu_opcode_DEC_rC,    cpu_opcode_LD_rC_u8,   cpu_opcode_RRCA,
-/* 0x1_ */ cpu_opcode_undefined, cpu_opcode_LD_rDE_u16, cpu_opcode_LD_iDE_rA,  cpu_opcode_INC_rDE,   cpu_opcode_INC_rD,      cpu_opcode_DEC_rD,    cpu_opcode_LD_rD_u8,  cpu_opcode_RLA,       cpu_opcode_JR_s8,     cpu_opcode_undefined, cpu_opcode_LD_rA_iDE,  cpu_opcode_undefined, cpu_opcode_INC_rE,     cpu_opcode_DEC_rE,    cpu_opcode_LD_rE_u8,   cpu_opcode_RRA,
-/* 0x2_ */ cpu_opcode_JR_NZ_s8,  cpu_opcode_LD_rHL_u16, cpu_opcode_LD_iHLi_rA, cpu_opcode_INC_rHL,   cpu_opcode_INC_rH,      cpu_opcode_DEC_rH,    cpu_opcode_LD_rH_u8,  cpu_opcode_undefined, cpu_opcode_JR_Z_s8,   cpu_opcode_undefined, cpu_opcode_LD_rA_iHLi, cpu_opcode_undefined, cpu_opcode_INC_rL,     cpu_opcode_DEC_rL,    cpu_opcode_LD_rL_u8,   cpu_opcode_undefined,
-/* 0x3_ */ cpu_opcode_JR_NC_s8,  cpu_opcode_LD_rSP_u16, cpu_opcode_LD_iHLd_rA, cpu_opcode_INC_rSP,   cpu_opcode_INC_iHL,     cpu_opcode_DEC_iHL,   cpu_opcode_LD_iHL_u8, cpu_opcode_undefined, cpu_opcode_JR_C_s8,   cpu_opcode_undefined, cpu_opcode_LD_rA_iHLd, cpu_opcode_undefined, cpu_opcode_INC_rA,     cpu_opcode_DEC_rA,    cpu_opcode_LD_rA_u8,   cpu_opcode_undefined,
-/* 0x4_ */ cpu_opcode_LD_rB_rB,  cpu_opcode_LD_rB_rC,   cpu_opcode_LD_rB_rD,   cpu_opcode_LD_rB_rE,  cpu_opcode_LD_rB_rH,    cpu_opcode_LD_rB_rL,  cpu_opcode_LD_rB_iHL, cpu_opcode_LD_rB_rA,  cpu_opcode_LD_rC_rB,  cpu_opcode_LD_rC_rC,  cpu_opcode_LD_rC_rD,   cpu_opcode_LD_rC_rE,  cpu_opcode_LD_rC_rH,   cpu_opcode_LD_rC_rL,  cpu_opcode_LD_rC_iHL,  cpu_opcode_LD_rC_rA, 
-/* 0x5_ */ cpu_opcode_LD_rD_rB,  cpu_opcode_LD_rD_rC,   cpu_opcode_LD_rD_rD,   cpu_opcode_LD_rD_rE,  cpu_opcode_LD_rD_rH,    cpu_opcode_LD_rD_rL,  cpu_opcode_LD_rD_iHL, cpu_opcode_LD_rD_rA,  cpu_opcode_LD_rE_rB,  cpu_opcode_LD_rE_rC,  cpu_opcode_LD_rE_rD,   cpu_opcode_LD_rE_rE,  cpu_opcode_LD_rE_rH,   cpu_opcode_LD_rE_rL,  cpu_opcode_LD_rE_iHL,  cpu_opcode_LD_rE_rA, 
-/* 0x6_ */ cpu_opcode_LD_rH_rB,  cpu_opcode_LD_rH_rC,   cpu_opcode_LD_rH_rD,   cpu_opcode_LD_rH_rE,  cpu_opcode_LD_rH_rH,    cpu_opcode_LD_rH_rL,  cpu_opcode_LD_rH_iHL, cpu_opcode_LD_rH_rA,  cpu_opcode_LD_rL_rB,  cpu_opcode_LD_rL_rC,  cpu_opcode_LD_rL_rD,   cpu_opcode_LD_rL_rE,  cpu_opcode_LD_rL_rH,   cpu_opcode_LD_rL_rL,  cpu_opcode_LD_rL_iHL,  cpu_opcode_LD_rL_rA, 
-/* 0x7_ */ cpu_opcode_LD_iHL_rB, cpu_opcode_LD_iHL_rC,  cpu_opcode_LD_iHL_rD,  cpu_opcode_LD_iHL_rE, cpu_opcode_LD_iHL_rH,   cpu_opcode_LD_iHL_rL, cpu_opcode_undefined, cpu_opcode_LD_iHL_rA, cpu_opcode_LD_rA_rB,  cpu_opcode_LD_rA_rC,  cpu_opcode_LD_rA_rD,   cpu_opcode_LD_rA_rE,  cpu_opcode_LD_rA_rH,   cpu_opcode_LD_rA_rL,  cpu_opcode_LD_rA_iHL,  cpu_opcode_LD_rA_rA,
-/* 0x8_ */ cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_undefined,   cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined,
-/* 0x9_ */ cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_undefined,   cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined,
-/* 0xA_ */ cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_undefined,   cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_XOR_rA_rB, cpu_opcode_XOR_rA_rC, cpu_opcode_XOR_rA_rD,  cpu_opcode_XOR_rA_rE, cpu_opcode_XOR_rA_rH,  cpu_opcode_XOR_rA_rL, cpu_opcode_XOR_rA_iHL, cpu_opcode_XOR_rA_rA,
-/* 0xB_ */ cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_undefined,   cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_CP_rA_rB,  cpu_opcode_CP_rA_rC,  cpu_opcode_CP_rA_rD,   cpu_opcode_CP_rA_rE,  cpu_opcode_CP_rA_rH,   cpu_opcode_CP_rA_rL,  cpu_opcode_CP_rA_iHL,  cpu_opcode_CP_rA_rA,
-/* 0xC_ */ cpu_opcode_RET_NZ,    cpu_opcode_POP_rBC,    cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_CALL_NZ_u16, cpu_opcode_PUSH_rBC,  cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_RET_Z,     cpu_opcode_RET,       cpu_opcode_undefined,  cpu_opcode_CB,        cpu_opcode_CALL_Z_u16, cpu_opcode_CALL_u16,  cpu_opcode_undefined,  cpu_opcode_undefined,
-/* 0xD_ */ cpu_opcode_RET_NC,    cpu_opcode_POP_rDE,    cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_CALL_NC_u16, cpu_opcode_PUSH_rDE,  cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_RET_C,     cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_CALL_C_u16, cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined,
-/* 0xE_ */ cpu_opcode_LDH_u8_rA, cpu_opcode_POP_rHL,    cpu_opcode_LDH_rC_rA,  cpu_opcode_undefined, cpu_opcode_undefined,   cpu_opcode_PUSH_rHL,  cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_LD_i16_rA,  cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_XOR_rA_u8,  cpu_opcode_undefined,
-/* 0xF_ */ cpu_opcode_LDH_rA_u8, cpu_opcode_POP_rAF,    cpu_opcode_LDH_rA_rC,  cpu_opcode_undefined, cpu_opcode_undefined,   cpu_opcode_PUSH_rAF,  cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_LD_rA_i16,  cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_CP_rA_u8,   cpu_opcode_undefined
+        // 0x_0                | 0x_1                 | 0x_2                 | 0x_3                | 0x_4                  | 0x_5                | 0x_6                 | 0x_7                | 0x_8                | 0x_9                | 0x_A                 | 0x_B                | 0x_C                 | 0x_D                | 0x_E                 | 0x_F
+/* 0x0_ */ cpu_opcode_NOP,       cpu_opcode_LD_rBC_u16, cpu_opcode_LD_iBC_rA,  cpu_opcode_INC_rBC,   cpu_opcode_INC_rB,      cpu_opcode_DEC_rB,    cpu_opcode_LD_rB_u8,   cpu_opcode_RLCA,      cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_LD_rA_iBC,  cpu_opcode_undefined, cpu_opcode_INC_rC,     cpu_opcode_DEC_rC,    cpu_opcode_LD_rC_u8,   cpu_opcode_RRCA,
+/* 0x1_ */ cpu_opcode_undefined, cpu_opcode_LD_rDE_u16, cpu_opcode_LD_iDE_rA,  cpu_opcode_INC_rDE,   cpu_opcode_INC_rD,      cpu_opcode_DEC_rD,    cpu_opcode_LD_rD_u8,   cpu_opcode_RLA,       cpu_opcode_JR_s8,     cpu_opcode_undefined, cpu_opcode_LD_rA_iDE,  cpu_opcode_undefined, cpu_opcode_INC_rE,     cpu_opcode_DEC_rE,    cpu_opcode_LD_rE_u8,   cpu_opcode_RRA,
+/* 0x2_ */ cpu_opcode_JR_NZ_s8,  cpu_opcode_LD_rHL_u16, cpu_opcode_LD_iHLi_rA, cpu_opcode_INC_rHL,   cpu_opcode_INC_rH,      cpu_opcode_DEC_rH,    cpu_opcode_LD_rH_u8,   cpu_opcode_undefined, cpu_opcode_JR_Z_s8,   cpu_opcode_undefined, cpu_opcode_LD_rA_iHLi, cpu_opcode_undefined, cpu_opcode_INC_rL,     cpu_opcode_DEC_rL,    cpu_opcode_LD_rL_u8,   cpu_opcode_undefined,
+/* 0x3_ */ cpu_opcode_JR_NC_s8,  cpu_opcode_LD_rSP_u16, cpu_opcode_LD_iHLd_rA, cpu_opcode_INC_rSP,   cpu_opcode_INC_iHL,     cpu_opcode_DEC_iHL,   cpu_opcode_LD_iHL_u8,  cpu_opcode_undefined, cpu_opcode_JR_C_s8,   cpu_opcode_undefined, cpu_opcode_LD_rA_iHLd, cpu_opcode_undefined, cpu_opcode_INC_rA,     cpu_opcode_DEC_rA,    cpu_opcode_LD_rA_u8,   cpu_opcode_undefined,
+/* 0x4_ */ cpu_opcode_LD_rB_rB,  cpu_opcode_LD_rB_rC,   cpu_opcode_LD_rB_rD,   cpu_opcode_LD_rB_rE,  cpu_opcode_LD_rB_rH,    cpu_opcode_LD_rB_rL,  cpu_opcode_LD_rB_iHL,  cpu_opcode_LD_rB_rA,  cpu_opcode_LD_rC_rB,  cpu_opcode_LD_rC_rC,  cpu_opcode_LD_rC_rD,   cpu_opcode_LD_rC_rE,  cpu_opcode_LD_rC_rH,   cpu_opcode_LD_rC_rL,  cpu_opcode_LD_rC_iHL,  cpu_opcode_LD_rC_rA, 
+/* 0x5_ */ cpu_opcode_LD_rD_rB,  cpu_opcode_LD_rD_rC,   cpu_opcode_LD_rD_rD,   cpu_opcode_LD_rD_rE,  cpu_opcode_LD_rD_rH,    cpu_opcode_LD_rD_rL,  cpu_opcode_LD_rD_iHL,  cpu_opcode_LD_rD_rA,  cpu_opcode_LD_rE_rB,  cpu_opcode_LD_rE_rC,  cpu_opcode_LD_rE_rD,   cpu_opcode_LD_rE_rE,  cpu_opcode_LD_rE_rH,   cpu_opcode_LD_rE_rL,  cpu_opcode_LD_rE_iHL,  cpu_opcode_LD_rE_rA, 
+/* 0x6_ */ cpu_opcode_LD_rH_rB,  cpu_opcode_LD_rH_rC,   cpu_opcode_LD_rH_rD,   cpu_opcode_LD_rH_rE,  cpu_opcode_LD_rH_rH,    cpu_opcode_LD_rH_rL,  cpu_opcode_LD_rH_iHL,  cpu_opcode_LD_rH_rA,  cpu_opcode_LD_rL_rB,  cpu_opcode_LD_rL_rC,  cpu_opcode_LD_rL_rD,   cpu_opcode_LD_rL_rE,  cpu_opcode_LD_rL_rH,   cpu_opcode_LD_rL_rL,  cpu_opcode_LD_rL_iHL,  cpu_opcode_LD_rL_rA, 
+/* 0x7_ */ cpu_opcode_LD_iHL_rB, cpu_opcode_LD_iHL_rC,  cpu_opcode_LD_iHL_rD,  cpu_opcode_LD_iHL_rE, cpu_opcode_LD_iHL_rH,   cpu_opcode_LD_iHL_rL, cpu_opcode_undefined,  cpu_opcode_LD_iHL_rA, cpu_opcode_LD_rA_rB,  cpu_opcode_LD_rA_rC,  cpu_opcode_LD_rA_rD,   cpu_opcode_LD_rA_rE,  cpu_opcode_LD_rA_rH,   cpu_opcode_LD_rA_rL,  cpu_opcode_LD_rA_iHL,  cpu_opcode_LD_rA_rA,
+/* 0x8_ */ cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_undefined,   cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined,
+/* 0x9_ */ cpu_opcode_SUB_rA_rB, cpu_opcode_SUB_rA_rC,  cpu_opcode_SUB_rA_rD,  cpu_opcode_SUB_rA_rE, cpu_opcode_SUB_rA_rH,   cpu_opcode_SUB_rA_rL, cpu_opcode_SUB_rA_iHL, cpu_opcode_SUB_rA_rA, cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined,
+/* 0xA_ */ cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_undefined,   cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_XOR_rA_rB, cpu_opcode_XOR_rA_rC, cpu_opcode_XOR_rA_rD,  cpu_opcode_XOR_rA_rE, cpu_opcode_XOR_rA_rH,  cpu_opcode_XOR_rA_rL, cpu_opcode_XOR_rA_iHL, cpu_opcode_XOR_rA_rA,
+/* 0xB_ */ cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_undefined,   cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_CP_rA_rB,  cpu_opcode_CP_rA_rC,  cpu_opcode_CP_rA_rD,   cpu_opcode_CP_rA_rE,  cpu_opcode_CP_rA_rH,   cpu_opcode_CP_rA_rL,  cpu_opcode_CP_rA_iHL,  cpu_opcode_CP_rA_rA,
+/* 0xC_ */ cpu_opcode_RET_NZ,    cpu_opcode_POP_rBC,    cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_CALL_NZ_u16, cpu_opcode_PUSH_rBC,  cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_RET_Z,     cpu_opcode_RET,       cpu_opcode_undefined,  cpu_opcode_CB,        cpu_opcode_CALL_Z_u16, cpu_opcode_CALL_u16,  cpu_opcode_undefined,  cpu_opcode_undefined,
+/* 0xD_ */ cpu_opcode_RET_NC,    cpu_opcode_POP_rDE,    cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_CALL_NC_u16, cpu_opcode_PUSH_rDE,  cpu_opcode_SUB_rA_u8,  cpu_opcode_undefined, cpu_opcode_RET_C,     cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_CALL_C_u16, cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined,
+/* 0xE_ */ cpu_opcode_LDH_u8_rA, cpu_opcode_POP_rHL,    cpu_opcode_LDH_rC_rA,  cpu_opcode_undefined, cpu_opcode_undefined,   cpu_opcode_PUSH_rHL,  cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_LD_i16_rA,  cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_XOR_rA_u8,  cpu_opcode_undefined,
+/* 0xF_ */ cpu_opcode_LDH_rA_u8, cpu_opcode_POP_rAF,    cpu_opcode_LDH_rA_rC,  cpu_opcode_undefined, cpu_opcode_undefined,   cpu_opcode_PUSH_rAF,  cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_undefined, cpu_opcode_LD_rA_i16,  cpu_opcode_undefined, cpu_opcode_undefined,  cpu_opcode_undefined, cpu_opcode_CP_rA_u8,   cpu_opcode_undefined
 };
 
 static const CPU_INSTR m_cb_opcode_table[0x100] = {
@@ -189,6 +189,12 @@ static inline void cpu_generic_DEC_r8(byte* dst) {
     struct alu_result result = alu_dec_8b(*dst);
     *dst                     = result.result.byte;
     registers_set_flags(&m_reg, result.flags.z, true, result.flags.h, registers_get_flag_c(&m_reg));
+}
+
+static inline void cpu_generic_SUB_r8_u8(byte* dst, const byte src) {
+    struct alu_result result = alu_sub_8b(*dst, src);
+    *dst                     = result.result.byte;
+    registers_set_flags(&m_reg, result.flags.z, true, result.flags.h, result.flags.c);
 }
 
 static inline void cpu_generic_XOR_r8_u8(byte* dst, const byte src) {
@@ -524,14 +530,16 @@ static void cpu_opcode_ADC_rA_iHL(void);
 static void cpu_opcode_ADC_rA_rA(void);
 
 // Subtract Register (or indirect HL) from A
-static void cpu_opcode_SUB_rA_rB(void);
-static void cpu_opcode_SUB_rA_rC(void);
-static void cpu_opcode_SUB_rA_rD(void);
-static void cpu_opcode_SUB_rA_rE(void);
-static void cpu_opcode_SUB_rA_rH(void);
-static void cpu_opcode_SUB_rA_rL(void);
-static void cpu_opcode_SUB_rA_iHL(void);
-static void cpu_opcode_SUB_rA_rA(void);
+static void cpu_opcode_SUB_rA_rB(void) { cpu_generic_SUB_r8_u8(&m_reg.A, m_reg.B); }
+static void cpu_opcode_SUB_rA_rC(void) { cpu_generic_SUB_r8_u8(&m_reg.A, m_reg.C); }
+static void cpu_opcode_SUB_rA_rD(void) { cpu_generic_SUB_r8_u8(&m_reg.A, m_reg.D); }
+static void cpu_opcode_SUB_rA_rE(void) { cpu_generic_SUB_r8_u8(&m_reg.A, m_reg.E); }
+static void cpu_opcode_SUB_rA_rH(void) { cpu_generic_SUB_r8_u8(&m_reg.A, m_reg.H); }
+static void cpu_opcode_SUB_rA_rL(void) { cpu_generic_SUB_r8_u8(&m_reg.A, m_reg.L); }
+static void cpu_opcode_SUB_rA_iHL(void) {
+    cpu_generic_SUB_r8_u8(&m_reg.A, mmu_read(BUS_CPU, m_reg.HL));
+}
+static void cpu_opcode_SUB_rA_rA(void) { cpu_generic_SUB_r8_u8(&m_reg.A, m_reg.A); }
 
 // Subtract with carry Register (or indirect HL) from A
 static void cpu_opcode_SBC_rA_rB(void);
@@ -590,7 +598,7 @@ static void cpu_opcode_CP_rA_rA(void) { cpu_generic_CP_r8_u8(&m_reg.A, m_reg.A);
 // Immediate ALU operation with A
 static void cpu_opcode_ADD_rA_u8(void);
 static void cpu_opcode_ADC_rA_u8(void);
-static void cpu_opcode_SUB_rA_u8(void);
+static void cpu_opcode_SUB_rA_u8(void) { cpu_generic_SUB_r8_u8(&m_reg.A, cpu_read_u8()); }
 static void cpu_opcode_SBC_rA_u8(void);
 static void cpu_opcode_AND_rA_u8(void);
 static void cpu_opcode_XOR_rA_u8(void) { cpu_generic_XOR_r8_u8(&m_reg.A, cpu_read_u8()); }
